13c88a26c66dbb09a7cbfd5a0b84af49906af9f9
grantgeorge <grantelliottgeorge@gmail.com>
2014-03-31 17:03:04 -0400

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('eventBudgetAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
