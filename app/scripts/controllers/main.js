cb90cd8a00ae9e226b8465e4356f846e083d8202
cfreeley <freeley.c@husky.neu.edu>
2014-04-28 01:11:28 -0400

    Started playing with app
    Really basic stuff to start (I just finished working through
    tutorials), gonna get serious after I wake up

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,4 +7,11 @@ angular.module('eventBudgetAppApp')
       'AngularJS',
       'Karma'
     ];
+
+    $scope.testdata = [
+	  	{name:'Pizza', estcost:10, actcost:9, quantity:30},
+  		{name:'Napkins', estcost:2, actcost:4, quantity:950},
+  		{name:'Cups', estcost:3, actcost:4, quantity:240}
+  	];
+  	
   });
